60ed51a9e00eb382d4ab6443556000b68a03105a
realestninja <the@realest.ninja>
2018-12-18 22:40:49 +0100

    Update behaviour

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-const removeElements = (elms) => [...elms].forEach(el => el.remove());
-removeElements( document.querySelectorAll(".PrM3e") );
+if(document.querySelectorAll(".PrM3e").length) {
+	const removeElements = (elms) => [...elms].forEach(el => el.remove());
+	removeElements( document.querySelectorAll(".PrM3e") );
+}
